717743a6684a581e4ca0ae1a0c7febe2f98da03b
Muso Nkuntsu <231223722@mycput.ac.za>
2026-04-04 21:42:58 +0200

    Initial commit

---
+++ src/main/java/za/ac/cput/traabco/booking/TraabcoBookingManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package za.ac.cput.traabco.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraabcoBookingManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraabcoBookingManagementSystemApplication.class, args);
+    }
+
+}
